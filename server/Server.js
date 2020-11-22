90642e7188b5374ce965e4aa2459c128596d44dd
89Jonask <72088076+89Jonask@users.noreply.github.com>
2020-11-22 19:15:15 +0100

    task4

--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,23 @@
-const express = require("express");
+import express, { response } from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import middlewares from "./src/middlewares/Middlewares.js";
 
+dotenv.config();
 const app = express();
+app.use(helmet());
+app.use(morgan("common"));
 
-app.listen(3001, () => {
-  console.log("servern är igång på port 3001");
+const port = process.env.PORT;
+
+app.get("/basket", (req, res) => {
+  res.send("gddassacxcfdfe");
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+app.listen(port, () => {
+  console.log(`servern är igång på port 3003 ${port}`);
 });
