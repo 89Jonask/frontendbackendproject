2185012684031efcabc2bbe236d29c6934937111
89Jonask <72088076+89Jonask@users.noreply.github.com>
2020-11-24 21:24:03 +0100

    mongodb

--- server/Server.js
+++ server/Server.js
@@ -3,21 +3,33 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import middlewares from "./src/middlewares/Middlewares.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
+const port = process.env.PORT;
+
 app.use(helmet());
 app.use(morgan("common"));
 
-const port = process.env.PORT;
-
-app.get("/basket", (req, res) => {
-  res.send("gddassacxcfdfe");
+app.get("/painting", (req, res) => {
+  res.send("painting");
 });
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
+mongoose
+  .connect("mongodb://localhost/nfgdatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Succesfully connected to the database!!!!"))
+  .catch((error) => {
+    console.log("ERROR WILE TRYING wTO CONNECT TO THE DATABASE: ", error);
+    process.exit();
+  });
+
 app.listen(port, () => {
-  console.log(`servern är igång på port 3003 ${port}`);
+  console.log(`servern är igång på portdd 3002 ${port}`);
 });
